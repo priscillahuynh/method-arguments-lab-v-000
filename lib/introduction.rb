0258d038db5334eca868e36be127380b7bcf65c0
Priscilla H <priscillamhuynh@gmail.com>
2018-06-19 08:25:46 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,9 @@
   end
 
   introduction("Josh")
+
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
+end
+
+introduction_with_language("Dan","Ember.js")
